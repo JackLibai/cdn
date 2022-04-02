d29ffaa0951bbfd6b6ce23b00e3aaa7a27b34aa3
workdesk <zhaojunbox@qq.com>
2022-04-02 11:49:11 +0800

    bat批处理自动推送:%date:~0,10%,%time:~0,8%

--- git一键上传.bat
+++ git一键上传.bat
@@ -3,10 +3,10 @@
 :: 获取当前脚本的路径
 cd /d %~dp0
 :: 自动提交
-git add . 
+git add --all 
  git commit -m "bat批处理自动推送:%date:~0,10%,%time:~0,8%" 
 ::  git commit -m "%commitMessage%" 
-git push origin main
+git push origin master
 @echo 已经完成,
 
 SET daoTime=60
